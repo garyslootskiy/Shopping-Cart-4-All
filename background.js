190a437fa9d74785d5d8ec10173a9fdd76688caa
garyslootskiy <69308215+garyslootskiy@users.noreply.github.com>
2020-09-23 21:05:12 -0400

    react 1

--- background.js
+++
@@ -1,8 +0,0 @@
-console.log('Background running');
-chrome.browserAction.onClicked.addListener(IconClicked);
-function IconClicked(tab) {
-  const msg = {
-    txt: 'Hello',
-  };
-  chrome.tabs.sendMessage(tab.id, msg);
-}
--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-function gotMessage(message, sender, sendresponse) {
-  console.log(message.txt);
-  const paragraphs = document.getElementsByTagName('p');
-  for (elt of paragraphs) {
-    elt.style['background-color'] = '#00CED1';
-  }
-}
