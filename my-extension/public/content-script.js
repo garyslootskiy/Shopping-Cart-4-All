6d1adca9d84e87388be1c4b846d84ddc7da409fb
garyslootskiy <69308215+garyslootskiy@users.noreply.github.com>
2020-09-23 23:36:28 -0400

    1136pm

---
+++ my-extension/public/content-script.js
@@ -0,0 +1,33 @@
+// //contentScript.js
+// const item = {};
+// item.name = document.getElementsByClassName("heading-5");
+// item.price = document.getElementsByClassName("priceView-customer-price");
+// return item;
+
+
+// var s = document.getElementById('stylehidecontrols');
+// if (s) {
+// s.remove();
+// }
+// else {
+// s = document.createElement('style');
+// s.id = 'stylehidecontrols';
+// var r = '#movie_player .ytp-gradient-top, #movie_player .ytp-gradient-bottom, #movie_player .ytp-chrome-top, #movie_player .ytp-progress-bar-container, #movie_player .ytp-chrome-controls';
+// s.appendChild(document.createTextNode(r));
+// document.body.appendChild(s);
+// }
+
+
+
+
+
+// let itemObject = {};
+// chrome.tabs.getSelected(null, () => {
+//   itemObject = getInputsByValue();
+// });
+// const getInputsByValue = (value) => {
+//   const item = {};
+//   item.name = document.getElementsByClassName("heading-5");
+//   item.price = document.getElementsByClassName("priceView-customer-price");
+//   return item;
+// };
